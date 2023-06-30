6949f564f1eb7089dc8e5b397f5903baa7ab422f
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-07-01 05:37:01 +0600

    added SocialLinks Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import SocialLinks from "./components/SocialLinks";
 
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Home></Home>
+      <SocialLinks></SocialLinks>
     </div>
   );
 };
